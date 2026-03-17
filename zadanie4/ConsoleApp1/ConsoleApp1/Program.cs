812cd084edf557858c80084633ff297c09dd6d83
unknown <boryskosa09@gmail.com>
2026-03-17 12:15:33 +0100

    zad 4 - sortowanie bombelkowe

---
+++ zadanie4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Gracz
+{
+    public string Imie;
+    public int Punkty;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Gracz[] gracze = {
+            new Gracz { Imie = "Kuba", Punkty = 12 },
+            new Gracz { Imie = "Ola", Punkty = 25 },
+            new Gracz { Imie = "Bartek", Punkty = 8 },
+            new Gracz { Imie = "Ania", Punkty = 19 },
+            new Gracz { Imie = "Mati", Punkty = 30 }
+        };
+
+        int porownania = 0;
+        int zamiany = 0;
+
+        Console.WriteLine("PRZED SORTOWANIEM:");
+        WypiszRanking(gracze);
+        
+        for (int i = 0; i < gracze.Length - 1; i++)
+        {
+            for (int j = 0; j < gracze.Length - 1 - i; j++)
+            {
+                porownania++;
+                
+                if (gracze[j].Punkty < gracze[j + 1].Punkty)
+                {
+                    Gracz temp = gracze[j];
+                    gracze[j] = gracze[j + 1];
+                    gracze[j + 1] = temp;
+                    zamiany++;
+                }
+            }
+            
+            Console.WriteLine($"\nRanking po przebiegu {i + 1}:");
+            WypiszRanking(gracze);
+        }
+
+        Console.WriteLine("\nPO SORTOWANIU:");
+        WypiszRanking(gracze);
+
+        // TODO 4 (na 4+): wypisz TOP 3
+        Console.WriteLine("\nTOP 3 gracze:");
+        for (int i = 0; i < 3 && i < gracze.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {gracze[i].Imie} - {gracze[i].Punkty} pkt");
+        }
+        
+        Console.WriteLine($"\nLiczba porównań: {porownania}");
+        Console.WriteLine($"Liczba zamian: {zamiany}");
+    }
+
+    static void WypiszRanking(Gracz[] gracze)
+    {
+        for (int i = 0; i < gracze.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {gracze[i].Imie} - {gracze[i].Punkty} pkt");
+        }
+    }
+}
